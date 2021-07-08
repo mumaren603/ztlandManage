1811e84de74b8bb7ecb241b9014bee41a23e9561
linshan liu <13338625696@163.com>
2021-07-08 17:46:00 +0800

    增加主机管理模块新增、编辑、删除url配置

--- hostManage/url.py
+++ hostManage/url.py
@@ -2,4 +2,7 @@ from django.urls import path,re_path
 from hostManage import views
 urlpatterns = [
     path('host',views.Host.as_view()),
+    path('host/add', views.hostAdd),
+    path('host/edit', views.hostEdit),
+    path('host/del', views.hostDel),
 ]
