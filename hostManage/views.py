484f65b99c825b0ceff4142602c1a1af556ed8a9
linshan liu <13338625696@163.com>
2021-07-08 17:46:26 +0800

    增加主机管理模块新增、编辑、删除业务代码

--- hostManage/views.py
+++ hostManage/views.py
@@ -3,20 +3,30 @@ from django.utils.decorators import method_decorator
 from hostManage import models
 from django.views import View
 import json
-from commonManage.views import getAuth,auth
+from commonManage.views import getAuth,auth,hostInfoValidTemplate
+from commonManage.logFunc import loggerConf
+from commonManage.views import Page
+logger = loggerConf().getLogger()
 
-# 主机管理
+# 查询主机信息
 @method_decorator(auth,name='dispatch')
 class Host(View):
     def get(self,request):
-        return render(request,'host.html')
+        # 获取权限
+        auth = getAuth(request.session.get('username'))
+        return render(request,'host.html',{'auth':auth})
 
     def post(self, request):
+        # 获取权限
+        auth = getAuth(request.session.get('username'))
+
+
         intranetIP = request.POST.get('intranetIP')
         extrantIP = request.POST.get('extrantIP')
         serverType = request.POST.get('serverType')
         serverOs = request.POST.get('serverOs')
 
+        # 根据前端传过来的不同条件加入到字典中
         queryConditions = {}
         if intranetIP:
             queryConditions['intranetIP'] = intranetIP
@@ -26,12 +36,13 @@ class Host(View):
             queryConditions['serverType'] = serverType
         if serverOs:
             queryConditions['serverOs'] = serverOs
-        print("查询条件是：", queryConditions)
+        logger.debug("查询条件：%s" % queryConditions)
 
+        # 将查询结果加入列表 返回给前端
         if queryConditions:
-            queryRes = models.ServerInfo.objects.filter(**queryConditions).all()
-            list = []
-            for i in queryRes:
+            queryHostRes = models.ServerInfo.objects.filter(**queryConditions).all()
+            queryHostResToList = []
+            for i in queryHostRes:
                 data = {
                     'id': i.id,
                     'intranetIP': i.intranetIP,
@@ -43,8 +54,114 @@ class Host(View):
                     'serverPassword': i.serverPassword,
                     'originServer': i.originServer
                 }
-                print('data', data)
-                list.append(data)
-            return HttpResponse(json.dumps(list))  # string
+                logger.debug("查询结果：%s" % data)
+                queryHostResToList.append(data)
+
+            return render(request,'host.html',{'queryHostResToList':queryHostResToList,'auth':auth})
         else:
-            print("请求参数不存在！")
+            logger.error("请求参数不存在！")
+
+# 编辑主机信息
+@auth
+def hostEdit(request):
+    if request.method == 'POST':
+        res_msg = {'status': 0, 'err_msg': None}
+
+        # id等获取前端传来的，其余字段可以从obj校验模板获取.obj封装前端需要校验的字段
+        id = request.POST.get('s_id')
+        serverPurpose = request.POST.get('serverPurpose')
+        originServer = request.POST.get('originServer')
+        extrantIP = request.POST.get('extrantIP')
+        if extrantIP is '':
+            extrantIP = None
+
+        obj = hostInfoValidTemplate(request.POST)  # 将前端校验数据发给hostInfoValidTemplate模板,模板中定义的字段开始做校验
+        v = obj.is_valid()
+        if v:
+            try:
+                intranetIP = obj.cleaned_data.get('intranetIP')
+                serverType = obj.cleaned_data.get('serverType')
+                serverOs = obj.cleaned_data.get('serverOs')
+                serverAccount = obj.cleaned_data.get('serverAccount')
+                serverPassword = obj.cleaned_data.get('serverPassword')
+                models.ServerInfo.objects.filter(id=id).update(
+                    intranetIP=intranetIP,
+                    extrantIP=extrantIP,
+                    serverType=serverType,
+                    serverOs=serverOs,
+                    serverPurpose=serverPurpose,
+                    originServer=originServer,
+                    serverAccount=serverAccount,
+                    serverPassword=serverPassword
+                )
+            except Exception as e:
+                res_msg['status'] = 1
+                res_msg['err_msg'] = '请求错误'
+                logger.error("请求异常：%s" % e)
+        else:
+            res_msg['status'] = 1
+            res_msg['err_msg'] = obj.errors
+        return HttpResponse(json.dumps(res_msg))  # string
+
+# 添加主机信息
+@auth
+def hostAdd(request):
+    if request.method == 'POST':
+        res_msg = {'status': 0, 'err_msg': None}
+
+        # id等获取前端传来的，其余字段可以从obj校验模板获取.obj封装前端需要校验的字段
+        serverPurpose = request.POST.get('serverPurpose')
+        originServer = request.POST.get('originServer')
+        extrantIP = request.POST.get('extrantIP')
+        logger.debug('extrantIP:', extrantIP)
+
+        obj = hostInfoValidTemplate(request.POST)  # 将前端校验数据发给hostInfoValidTemplate模板,模板中定义的字段开始做校验
+        v = obj.is_valid()
+        if v:
+            try:
+                intranetIP = obj.cleaned_data.get('intranetIP')
+                serverType = obj.cleaned_data.get('serverType')
+                serverOs = obj.cleaned_data.get('serverOs')
+                serverAccount = obj.cleaned_data.get('serverAccount')
+                serverPassword = obj.cleaned_data.get('serverPassword')
+                models.ServerInfo.objects.create(
+                    intranetIP=intranetIP,
+                    extrantIP=extrantIP,
+                    serverType=serverType,
+                    serverOs=serverOs,
+                    serverPurpose=serverPurpose,
+                    originServer=originServer,
+                    serverAccount=serverAccount,
+                    serverPassword=serverPassword
+                )
+            except Exception as e:
+                res_msg['status'] = 1
+                res_msg['err_msg'] = '请求错误'
+                logger.error("请求异常：%s" % e)
+        else:
+            res_msg['status'] = 1
+            res_msg['err_msg'] = obj.errors
+        return HttpResponse(json.dumps(res_msg))  # string
+
+# 删除主机信息
+@auth
+def hostDel(request):
+    if request.method == 'POST':
+        res_msg = {'status': 0, 'err_msg': None}
+
+        hid = request.POST.get('hid')
+        logger.debug('删除主机id为：%s' %hid)
+
+        if hid:
+            try:
+                # 删除操作
+                models.ServerInfo.objects.filter(id=hid).delete()
+            except Exception as e:
+                logger.error("请求异常：%s" %e)
+                res_msg['status'] = 1
+                res_msg['err_msg'] = '请求错误'
+        else:
+            logger.error("未获取到删除表主键id：%d" %hid)
+            res_msg['status'] = 1
+            res_msg['err_msg'] = '未获取到删除表主键id'
+        return HttpResponse(json.dumps(res_msg))
